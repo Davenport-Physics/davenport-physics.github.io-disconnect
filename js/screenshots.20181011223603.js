2e96b52e7464df68092c1916c14472c1ff34e71f
Emma Davenport <Davenport.Physics@gmail.com>
2018-10-11 22:36:29 -0500

    Updated website

---
+++ js/screenshots.20181011223603.js
@@ -0,0 +1,12 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;var a='data-src'; if($('.js3 .slide0').hasAttr('src')) { a='src'; } $('.js3 .slide0').attr(a, (dpi>1) ? 'images/example-1000.png' : 'images/example-500.png');
+var a='data-src'; if($('.js3 .slide1').hasAttr('src')) { a='src'; } $('.js3 .slide1').attr(a, (dpi>1) ? 'images/example1-1000.png' : 'images/example1-500.png');};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+initMenu($('#m1')[0]);
+$('.js3 .slider').slick({lazyLoad: 'ondemand',slidesToShow: 1,slidesToScroll: 1,fade: true,slide: 'div',cssEase: 'linear',speed: 300,dots: false,arrows: true,infinite: true});
+});
